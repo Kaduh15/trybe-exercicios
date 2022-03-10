31b186c0e17a1bef554b933a2dee8e460cf943ff
Kadu <kadu.silva2014@gmail.com>
2022-03-10 16:30:32 -0300

    resolve bonus03

--- M1-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
+++ M1-Fundamentos/bloco-4-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/bonus.js
@@ -19,3 +19,43 @@ function verificaPalindrome(palavra) {
         return false
     }
 }
+
+// 2 - Crie uma função que receba um array de inteiros e retorne o índice do maior valor.
+// Array de teste: [2, 3, 6, 7, 10, 1]; .
+// Valor esperado no retorno da função: 4 .
+
+let array = [2, 3, 6, 7, 10, 1];
+
+
+console.log(maiorNumero(array))
+
+
+function maiorNumero(array) {
+    let maior = array[0]
+    for (let value of array) {
+        if (value > maior) {
+            maior = value
+        }
+    }
+    return array.indexOf(maior)
+}
+
+// 3 - Crie uma função que receba um array de inteiros e retorne o índice do menor valor.
+// Array de teste: [2, 4, 6, 7, 10, 0, -3]; .
+// Valor esperado no retorno da função: 6 .
+
+let array2 = [2, 4, 6, 7, 10, 0, -3];
+
+
+console.log(maiorNumero(array2))
+
+
+function maiorNumero(array) {
+    let menor = array[0]
+    for (let value of array) {
+        if (value < menor) {
+            menor = value
+        }
+    }
+    return array.indexOf(menor)
+}
