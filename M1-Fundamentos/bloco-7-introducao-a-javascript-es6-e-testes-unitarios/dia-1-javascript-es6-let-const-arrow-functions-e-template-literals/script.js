a9df9f458c5e330e0c34677c07dcf814c00a6834
Kadu <kadu.silva2014@gmail.com>
2022-04-01 14:34:40 -0300

    resolve requisito 2 da parte 1

--- M1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
+++ M1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/script.js
@@ -1,13 +1,22 @@
-function testingScope(escopo) {
-    if (escopo === true) {
-      let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-      ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
-      console.log(ifScope);
-    } else {
-      const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-      console.log(elseScope);
-    }
-    // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
-  }
+// Exercício 1
 
-  testingScope(false);
+function testingScope(escopo) {
+  if (escopo === true) {
+    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+    ifScope = ifScope + ' ótimo, fui utilizada no escopo !';
+    console.log(ifScope);
+  } else {
+    const elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+    console.log(elseScope);
+  }
+  // console.log(ifScope + ' o que estou fazendo aqui ? :O'); // Se necessário esta linha pode ser removida.
+}
+
+testingScope(false);
+
+// Exercício 2
+
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+console.log(`Os números ${oddsAndEvens.sort((a, b) => a - b)} se encontram em forma crescente!`); // será necessário alterar essa linha 😉
+
