9c72c9ca108472960c0c4a3943ec87fc2a50571b
Kadu <kadu.silva2014@gmail.com>
2022-04-05 15:44:24 -0300

    resolve requesito 2 do execicio 1

--- M1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ M1-Fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -3,5 +3,8 @@ const sum = require('./sum');
 describe('1 - A função sum(a, b) retorna a soma do parâmetro a com o b', () => {
   it('1 - Teste se o retorno de sum(4, 5) é 9', () => {
     expect(9).toEqual(sum(4, 5));
-  })
+  });
+  it('2 - Teste se o retorno de sum(0, 0) é 0', () => {
+    expect(0).toEqual(sum(0, 0))
+  });
 });
