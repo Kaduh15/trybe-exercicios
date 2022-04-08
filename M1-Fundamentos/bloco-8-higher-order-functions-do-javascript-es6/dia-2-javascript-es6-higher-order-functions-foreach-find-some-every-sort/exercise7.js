fdc2e3ff601521179fb2ac3ab3eb6337e34c998d
Kadu <kadu.silva2014@gmail.com>
2022-04-08 15:33:40 -0300

    resolve o exercicio 7 do dia e termina todos

---
+++ M1-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-2-javascript-es6-higher-order-functions-foreach-find-some-every-sort/exercise7.js
@@ -0,0 +1,80 @@
+const books = [{
+  id: 1,
+  name: 'As Crônicas de Gelo e Fogo',
+  genre: 'Fantasia',
+  author: {
+    name: 'George R. R. Martin',
+    birthYear: 1948,
+  },
+  releaseYear: 1991,
+},
+{
+  id: 2,
+  name: 'O Senhor dos Anéis',
+  genre: 'Fantasia',
+  author: {
+    name: 'J. R. R. Tolkien',
+    birthYear: 1892,
+  },
+  releaseYear: 1954,
+},
+{
+  id: 3,
+  name: 'Fundação',
+  genre: 'Ficção Científica',
+  author: {
+    name: 'Isaac Asimov',
+    birthYear: 1920,
+  },
+  releaseYear: 1951,
+},
+{
+  id: 4,
+  name: 'Duna',
+  genre: 'Ficção Científica',
+  author: {
+    name: 'Frank Herbert',
+    birthYear: 1920,
+  },
+  releaseYear: 1965,
+},
+{
+  id: 5,
+  name: 'A Coisa',
+  genre: 'Terror',
+  author: {
+    name: 'Stephen King',
+    birthYear: 1947,
+  },
+  releaseYear: 1986,
+},
+{
+  id: 6,
+  name: 'O Chamado de Cthulhu',
+  genre: 'Terror',
+  author: {
+    name: 'H. P. Lovecraft',
+    birthYear: 1890,
+  },
+  releaseYear: 1928,
+},
+];
+
+// 7 - Faça uma função que retorne true , caso nenhum author tenha nascido no mesmo ano, e false , caso contrário.
+
+const expectedResult = false;
+
+function authorUnique() {
+  let birthYear = []
+  books.forEach((elemento) => birthYear.push(elemento.author.birthYear))
+  birthYear.sort((a, b) => a - b)
+  birthYear.forEach((elemento, index) => {
+    if (elemento === birthYear[index + 1]) {
+      birthYear = true
+      return birthYear
+    }
+  });
+  return birthYear
+}
+
+console.log(authorUnique());
