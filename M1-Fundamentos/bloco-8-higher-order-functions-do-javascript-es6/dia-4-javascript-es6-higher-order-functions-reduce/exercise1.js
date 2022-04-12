48b3efd51890610cd88ef169585454dce91270a0
Kadu <kadu.silva2014@gmail.com>
2022-04-12 14:32:28 -0300

    resolve o exercicio 2 do dia

---
+++ M1-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise1.js
@@ -0,0 +1,16 @@
+const arrays = [
+  ['1', '2', '3'],
+  [true],
+  [4, 5, 6],
+];
+
+// 1 - Dada uma matriz, transforme em um array.
+
+function flatten() {
+  console.log(arrays);
+  return arrays.reduce((arrayAtual, array) => {
+    arrayAtual
+  }, [])
+}
+
+console.log(flatten());
