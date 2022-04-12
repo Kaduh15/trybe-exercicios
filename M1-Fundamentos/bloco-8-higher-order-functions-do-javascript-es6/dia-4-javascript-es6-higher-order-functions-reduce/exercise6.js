2124f172c7a7bdf2f7988b0f2f50b77dc0a3db5f
Kadu <kadu.silva2014@gmail.com>
2022-04-12 18:21:47 -0300

    resolve o exercicio 6 do dia e termina todos

--- M1-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
+++ M1-Fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-4-javascript-es6-higher-order-functions-reduce/exercise6.js
@@ -1,21 +1,31 @@
-const names = [
-  'Aanemarie', 'Adervandes', 'Akifusa',
-  'Abegildo', 'Adicellia', 'Aladonata',
-  'Abeladerco', 'Adieidy', 'Alarucha',
-];
+// 6. - Agora vamos criar um novo array de objetos a partir das informações abaixo, onde cada objeto terá o formato { name: nome do aluno, average: media das notas } . Para isso vamos assumir que a posição 0 de notas refere-se ao aluno na posição 0 de alunos , aqui além de reduce será necessário utilizar também a função map . Dica: Você pode acessar o index do array dentro de map , e você pode ver o objeto esperado na constante expected .
 
-function containsA() {
-  return names.reduce((as, nomeAtual) => {
-    const nomeSplitado = nomeAtual.toLowerCase().split('');
-    
-    nomeSplitado.forEach(letra => {
-      if (letra === 'a') {
-        as += 1
-      }
-    });
+const students = ['Pedro Henrique', 'Miguel', 'Maria Clara'];
+const grades = [[9, 8, 10, 7, 5], [10, 9, 9, 10, 8], [10, 7, 10, 8, 9]];
 
-    return as
-  }, 0)
+function studentAverage() {
+  const newArrayMedias = grades.map((notasAluno) => {
+    const somaNotas = notasAluno.reduce((acc, nota) => {
+      return acc + nota
+    }, 0);
+
+    return somaNotas / notasAluno.length
+  })
+
+  const arrayAlunosNotas = students.map((aluno, index) => {
+    const obj = {}
+    obj.name = aluno
+    obj.average = newArrayMedias[index]
+    return obj
+  })
+  
+  return arrayAlunosNotas
 }
 
-console.log(containsA());
+const expected = [
+  { name: 'Pedro Henrique', average: 7.8 },
+  { name: 'Miguel', average: 9.2 },
+  { name: 'Maria Clara', average: 8.8 },
+];
+
+console.log(studentAverage());
